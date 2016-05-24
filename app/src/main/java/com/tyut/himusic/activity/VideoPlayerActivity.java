f8c6d1a4ff9fbaa92758b249eab959840dfc5082
LuQingchuan <luqingchuan@issuebang.com>
2016-05-24 16:28:45 +0800

    [UPDATE]

---
+++ app/src/main/java/com/tyut/himusic/activity/VideoPlayerActivity.java
@@ -0,0 +1,30 @@
+package com.tyut.himusic.activity;
+
+import android.os.Bundle;
+
+import com.tyut.himusic.R;
+
+public class VideoPlayerActivity extends BashActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+        initView();
+        initData();
+    }
+
+    @Override
+    protected void initView()
+    {
+
+    }
+
+    @Override
+    protected void initData()
+    {
+
+    }
+}
